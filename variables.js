9cdda88ee59d9fe5ee97bc32e8ccdda0315badd0
Khaja Moinuddin <103602186+khajamoi@users.noreply.github.com>
2024-11-02 06:27:06 +0000

    variables.js

---
+++ variables.js
@@ -0,0 +1,11 @@
+const accountID =2233234
+let accountEmail ="khaja@gmail.com"
+var accountpassowrd ="12432"
+accountCity ="jaipur"
+let accountState;
+
+accountEmail ="ok@gmail.com"
+accountpassowrd ="726387"
+accountCity ="delhi"
+
+console.table([accountID,accountEmail,accountpassowrd,accountCity,accountState]);
